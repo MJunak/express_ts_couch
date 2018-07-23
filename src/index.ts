6ca92e1179305619b07177a134acfcb600161fb8
sfuser <marius.junak@scorpius-forge.de>
2018-07-23 09:08:38 +0200

    Added Couchdb

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,23 @@ import * as bodyParser from "body-parser";
 import * as express from "express";
 import { Request, Response } from "express";
 import * as http from 'http';
+var nano = require('nano')('http://localhost:5984'); // Connect to the CouchDB running on port 5984
+var db_name = 'test'; // The name of the database to connect to
+var db = nano.use(db_name);
+
 
 var app:express.Application = express();
 
 app.get('/', (req: Request, res: Response) => {
+
+   
   res.status(200).json({status: "ok"});
+});
+
+app.get('/testDb',(req: Request, res: Response) => {
+  db.list({ include_docs: true }, function(err, body){
+		res.send(body);
+	});
 });
 
 
